2bf67d7deaeef0db14228ea75564208cf985e020
Pablo <pablo@intvs.org>
2019-10-06 17:02:40 +0100

    Create Layout Component

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,37 @@
+import React, {Fragment, Component} from 'react'
+import style from './Layout.module.css'
+import Toolbar from '../Navagation/Toolbar/Toolbar'
+import SideDrawer from '../Navagation/SideDrawer/SideDrawer'
+
+class Layout extends Component {
+
+  state = {
+    showSideDrawer:true
+  }
+
+  sideDrawerCloseHandler = () => {
+    this.setState({showSideDrawer:false})
+  }
+
+  sideDrawerOpenHandler = () => {
+    this.setState({showSideDrawer:true})
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <Toolbar open={this.sideDrawerOpenHandler} />
+        <SideDrawer 
+        closed={this.sideDrawerCloseHandler}
+        open={this.state.showSideDrawer}
+        />
+      <main className={style.Main}>
+        {this.props.children}
+      </main>
+      </Fragment>
+    )
+  }
+
+}
+
+export default Layout

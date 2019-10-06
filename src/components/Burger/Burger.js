99723f8acb58569d4be73baa31d59c220af2d10a
Pablo <pablo@intvs.org>
2019-10-06 16:59:50 +0100

    Create Burger Module

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
+import style from './Burger.module.css'
+
+const Burger = props => {
+ 
+  const transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => [...Array(props.ingredients[igKey])]
+        .map((_, i)=> <BurgerIngredient key={igKey + i} type={igKey} />
+      )
+    )
+  return (
+    <div className={style.Burger}>
+      <BurgerIngredient type='bread-top'/>
+      {props.sum !== 0 ? transformedIngredients : <p>Please make your order</p>}
+      <BurgerIngredient type='bread-bottom'/>
+    </div>
+  )
+}
+
+
+export default Burger;

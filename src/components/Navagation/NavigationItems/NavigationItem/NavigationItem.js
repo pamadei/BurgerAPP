4912eba3087de89bf8209ca1b49bfcd82cc94872
Pablo <pablo@intvs.org>
2019-10-09 16:11:53 +0100

    Add router and remove active from link

--- src/components/Navagation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navagation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import style from './NavigationItem.module.css'
+import {Link} from 'react-router-dom'
 
 const NavigationItem = props => {
 
   return (
     <li className={style.Item}>
-    <a 
-    href={props.link}
+    <Link 
+    to={props.link}
     className={props.active ? style.active : null }
-    >{props.children}</a></li>
+    >{props.children}</Link></li>
   )
 }
 
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 import Layout from '../components/Layout/Layout'
 import BurgerBuilder from './BurgerBuilder/BurgerBuilder'
 import Orders from '../containers/Orders/Orders'
+import OrderPage from '../components/OrderPage/OrderPage'
 import {BrowserRouter} from 'react-router-dom'
 import {Route} from 'react-router-dom'
+
 
 class App extends Component {
   render() {
@@ -13,6 +15,7 @@ class App extends Component {
         <Layout>
         <Route path="/" exact component={BurgerBuilder}/>
         <Route path="/Orders" exact component={Orders}/>
+        <Route path="/Orders/:id" exact component={OrderPage}/>
         </Layout>
         </BrowserRouter>
       </div>

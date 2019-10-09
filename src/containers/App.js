07becff48628930a4fc9180670b57a658af3d77f
Pablo <pablo@intvs.org>
2019-10-09 16:59:53 +0100

    Add 404 Page

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,7 +4,8 @@ import BurgerBuilder from './BurgerBuilder/BurgerBuilder'
 import Orders from '../containers/Orders/Orders'
 import OrderPage from '../components/OrderPage/OrderPage'
 import {BrowserRouter} from 'react-router-dom'
-import {Route} from 'react-router-dom'
+import {Route, Switch} from 'react-router-dom'
+import Checkout from '../components/Checkout/Checkout'
 
 
 class App extends Component {
@@ -13,9 +14,13 @@ class App extends Component {
       <div>
         <BrowserRouter>
         <Layout>
-        <Route path="/" exact component={BurgerBuilder}/>
-        <Route path="/Orders" exact component={Orders}/>
-        <Route path="/Orders/:id" exact component={OrderPage}/>
+          <Switch>
+            <Route path="/Orders" exact component={Orders}/>
+            <Route path="/Orders/:id" exact component={OrderPage}/>
+            <Route path="/Checkout" exact component={Checkout}/>
+            <Route path="/" exact component={BurgerBuilder}/>
+            <Route render={()=> <h1 style={{textAlign:"center"}}>404 Page</h1>}/>
+          </Switch>
         </Layout>
         </BrowserRouter>
       </div>

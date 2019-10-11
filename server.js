1dc70cc705de65658fa2b967318de647156f4a0d
Pablo <pablo@intvs.org>
2019-10-11 20:30:39 +0100

    Update Server

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.use('/api/orders', orders);
 // Serve static assets if in Production
 if(process.env.NODE_ENV === 'production'){
   // Set static folder
-  app.use(express.static('/frontend/build'));
+  app.use(express.static('frontend/build'));
   
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));

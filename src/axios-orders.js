21a080127ff20be8435f72d62df6378970d1c3a4
Pablo <pablo@intvs.org>
2019-10-11 11:43:41 +0100

    Update axios instance for Prod

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
+let development = process.env.NODE_ENV !== 'production'
+
 const instance = axios.create({
-  baseURL: 'http://localhost:5000'
+  baseURL: development ? 'http://localhost:5000' : '/'
 })
 
 export default instance;

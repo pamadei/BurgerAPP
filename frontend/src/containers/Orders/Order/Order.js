7551a56c61ae44dede2a620736995b44dc2cd887
Pablo <pablo@intvs.org>
2019-10-12 21:08:43 +0100

    Add Order and Date

--- frontend/src/containers/Orders/Order/Order.js
+++ frontend/src/containers/Orders/Order/Order.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import style from './Order.module.css'
+import {format} from 'date-fns'
 
 const Order = props => {
 
   return (
     <li className={style.Order}>
+      <p>Order: {props.order.orderNumber}</p>
+      <p>Date: {format(new Date(props.order.date), "yyyy-MM-dd' - 'HH:mm aaaa")
+}</p>
       Ingredients: {props.ingredientsSummary}
-      <p>Price: £<span style={{fontWeight:'bold'}}>{props.order.totalPrice}</span>
+      <p>Price: £<span style={{fontWeight:'bold'}}>{props.order.totalPrice.toFixed(2)}</span>
       </p>
       <p>Customer Name: {props.order.customer.name} {props.order.customer.lastName}</p>
     </li>

5a7b98027221485d2fcfb19a86f7cd6b07a51873
Pablo <pablo@intvs.org>
2019-10-12 18:59:09 +0100

    Update Spinner

--- frontend/src/components/UI/Spinner/Spinner.js
+++ frontend/src/components/UI/Spinner/Spinner.js
@@ -2,6 +2,9 @@ import React from 'react'
 import style from './Spinner.module.css'
 
 
-const Spinner = props => <div className={style.Loader}></div>
+const Spinner = props => {
+ 
+  return <div className={style.Loader}></div>
+}
 
 export default Spinner

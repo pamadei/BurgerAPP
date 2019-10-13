972de27a100ce4067ab72b9ab25b7cd12ca73619
Pablo <pablo@intvs.org>
2019-10-13 17:58:31 +0100

    Add style to Orders

--- frontend/src/containers/Orders/OrderIngredient/OrderIngredient.js
+++ frontend/src/containers/Orders/OrderIngredient/OrderIngredient.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const OrderIngredient = props => {
   return (
-    <span style={{marginLeft:'0.5rem'}}>
+    <span style={{marginLeft:'0.5rem', textTransform:'capitalize'}}>
     {props.ingredient} ({props.ingQuantity})
     </span>
   )

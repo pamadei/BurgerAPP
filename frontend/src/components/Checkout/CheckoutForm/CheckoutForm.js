eab4238cf0a6bf42a578c38263eb67ea7f402a79
Pablo <pablo@intvs.org>
2019-10-13 18:17:22 +0100

    Amend Placeholder

--- frontend/src/components/Checkout/CheckoutForm/CheckoutForm.js
+++ frontend/src/components/Checkout/CheckoutForm/CheckoutForm.js
@@ -7,7 +7,7 @@ const CheckoutForm = props => {
       <form className={style.Form} onSubmit={props.submitForm}>
         <input className={style.Input} type="text" name="name"  value={props.customer.name} onChange={props.changeHandler} placeholder="First Name"/>
         <input className={style.Input} type="text" name="lastName"  value={props.customer.lastName}onChange={props.changeHandler}  placeholder="Last Name"/>
-        <input className={style.Input} type="email" name="email" value={props.customer.email} onChange={props.changeHandler}  placeholder="Email Name"/>
+        <input className={style.Input} type="email" name="email" value={props.customer.email} onChange={props.changeHandler}  placeholder="Email"/>
         <button className={style.Submit} type="submit">Place your Order</button>
       </form>
     </div>
